3cf0eaa7c4d0f26155ce975ae6c448e07c9f63f9
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-05-01 04:09:54 -0500

    Search delegate mock.

---
+++ lib/ui/delegates/search_marker_delegate.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SearchMarkerDelegate extends SearchDelegate {
+
+  SearchMarkerDelegate():super(
+    searchFieldLabel: 'Buscar...'
+  );
+  
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(
+          Icons.clear
+        ),
+        onPressed: () => query = ''
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(
+        Icons.arrow_back_rounded
+      ),
+      onPressed: () {
+        close(context, null);
+      }
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: const Icon(
+            Icons.location_on_outlined,
+            color: Colors.black,
+          ),
+          title: Text(""),
+          onTap: () {},
+        )
+      ],
+    );
+  }
+
+}
